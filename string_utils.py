ed8aa61ee2f0159194d61c146c8fafacf8e06116
322838863 <shovalgos@gmail.com>
2025-11-14 14:49:12 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -3,21 +3,7 @@ def split_before_each_uppercases(input):
     Splits a string before every uppercase character.
     Uppercase letters remain in the resulting parts.
     """
-    if not input:
-        return []
-
-    parts = []
-    current = input[0]
-
-    for ch in input[1:]:
-        if ch.isupper():
-            parts.append(current)
-            current = ch
-        else:
-            current += ch
-
-    parts.append(current)
-    return parts
+    
 
 def split_at_first_digit(s):
     """
@@ -28,12 +14,4 @@ def split_at_first_digit(s):
     If no digit exists:
         return (original_string, 1)
     """
-    for i, ch in enumerate(s):
-        if ch.isdigit():
-            # First digit found
-            prefix = s[:i]
-            number = int(s[i:])
-            return prefix, number
-
-    # No digit found
-    return s, 1
+  

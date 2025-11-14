7cea6a0df0ea97b2f994a58d3765c6e1cf76ee3d
322838863 <shovalgos@gmail.com>
2025-11-14 22:00:18 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,11 +1,11 @@
-def split_before_each_uppercases(input):    
-    if not input:
+def split_before_each_uppercases(formual):    
+    if not formual:
         return []
 
     parts = []
-    current = input[0]
+    current = formual[0]
 
-    for ch in input[1:]:
+    for ch in formual[1:]:
         if ch.isupper():
             parts.append(current)
             current = ch
@@ -15,13 +15,13 @@ def split_before_each_uppercases(input):
     parts.append(current)
     return parts
 
-def split_at_first_digit(s):    
-    for i, ch in enumerate(s):
+def split_at_first_digit(formual):    
+    for i, ch in enumerate(formual):
         if ch.isdigit():
             # First digit found
-            prefix = s[:i]
-            number = int(s[i:])
-            return prefix, number
+            prefix = formual[:i]
+            number = formual[i:]
+            return prefix, int(number)
 
     # No digit found
     return s, 1
